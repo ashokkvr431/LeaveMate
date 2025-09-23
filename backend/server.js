4fc3b6586a4de222a1d36bc4be5069f06796aca9
ashokkvr431 <ashokkvr106@gmail.com>
2025-09-23 10:45:50 +0530

    remove unwanted code

--- backend/server.js
+++ backend/server.js
@@ -37,34 +37,3 @@ app.listen(PORT, "0.0.0.0", () =>
     console.log(`🚀 Server running on http://0.0.0.0:${PORT}`)
 );
 
-
-
-// require('dotenv').config();
-// const express = require('express');
-// const cors = require('cors');
-// const path = require('path');
-
-// const authRoutes = require('./routes/authRoutes');
-// const profileRoutes = require('./routes/profileRoutes');
-// const userRoutes = require('./routes/userRoutes');
-// const leaveRoutes = require('./routes/leaveRoutes');
-// const attendanceRoutes = require('./routes/attendanceRoutes');
-
-// const app = express();
-
-// app.use(cors({ origin: '*', allowedHeaders: ['Content-Type', 'Authorization'] }));
-// app.use(express.json());
-// app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
-// app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
-
-
-// app.use('/api/auth', authRoutes);
-// app.use('/api/profile', profileRoutes);
-// app.use('/api/users', userRoutes);
-// app.use('/api/leaves', leaveRoutes);
-// app.use('/api/attendance', attendanceRoutes);
-
-// app.get('/', (_req, res) => res.send('LeaveMate API running 🚀'));
-
-// const PORT = process.env.PORT || 3000;
-// app.listen(PORT, '0.0.0.0', () => console.log(`🚀 Server running on http://0.0.0.0:${PORT}`));
