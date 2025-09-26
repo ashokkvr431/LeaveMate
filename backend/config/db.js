e13adf38def55af5c6d867b2f220cb44d3edf9b9
ashokkvr431 <ashokkvr106@gmail.com>
2025-09-26 11:18:34 +0530

    Updated Code

--- backend/config/db.js
+++ backend/config/db.js
@@ -16,23 +16,3 @@ db.getConnection()
   .catch(err => console.error("MySQL connection failed:", err));
 
 module.exports = db;
-
-
-
-
-
-
-
-// const mysql = require('mysql2');
-// require('dotenv').config();
-
-// const pool = mysql.createPool({
-//   host: process.env.DB_HOST || '127.0.0.1',
-//   user: process.env.DB_USER || 'root',
-//   password: process.env.DB_PASS || '',
-//   database: process.env.DB_NAME || 'leave_attendance',
-//   waitForConnections: true,
-//   connectionLimit: 10
-// });
-
-// module.exports = pool.promise();
